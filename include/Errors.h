9c63d170b9c4a4b4ced115e0919b346c612a6f74
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-09 21:16:45 +0300

    Added more errors

--- include/Errors.h
+++ include/Errors.h
@@ -8,7 +8,14 @@
 
 enum PhiChatErrors
 {
-    NO_ERROR = 0
+    NO_ERROR                    =  0,
+    POINTER_NULL                = -1,
+    CLIENT_NULL                 = -2,
+    NAME_NULL                   = -3,
+    NAME_TOO_LONG               = -4,
+    INVALID_ID                  = -5,
+    CLIENT_ALREADY_LOGGED       = -6,
+    CLIENT_NOT_LOGGED           = -7
 };
 
 #endif
