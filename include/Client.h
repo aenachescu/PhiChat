29208145827fb46a8fa1610ef26c44466bf98bc7
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-09 21:18:03 +0300

    Added the declarations of helper functions for struct Client

--- include/Client.h
+++ include/Client.h
@@ -19,5 +19,16 @@ struct Client
     uint64_t id;
 };
 
+enum PhiChatErrors NewClient(__OUT__ struct Client**,
+                             __IN__  const char *name,
+                             __IN__  const uint32_t socket);
+
+enum PhiChatErrors DeleteClient(__IN__ struct Client**);
+
+enum PhiChatErrors Login(__IN__ struct Client*,
+                         __IN__ const uint64_t id);
+
+enum PhiChatErrors Logout(__IN__ struct Client*);
+
 #endif
 
