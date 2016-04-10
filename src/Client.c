8d3d60900a60fff794a663c21da550273b19dff4
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-10 12:02:15 +0300

    Modified NewClient() and Login()

--- src/Client.c
+++ src/Client.c
@@ -9,29 +9,21 @@
 #include <string.h>
 
 enum PhiChatErrors NewClient(__OUT__ struct Client **newClient,
-                             __IN__  const char *name,
                              __IN__  const uint32_t socket)
 {
     if (newClient == NULL)
         return POINTER_NULL;
-    if (name == NULL)
-        return NAME_NULL;
     if (socket == 0)
         return INVALID_SOCKET;
-
-    size_t length = strlen(name);
-
-    if (length >= MAX_LENGTH_NAME)
-        return NAME_TOO_LONG;
 
     *newClient = (struct Client*) malloc(sizeof(struct Client));
     if (*newClient == NULL)
         return CLIENT_NULL;
 
-    (*newClient)->socket = socket;
-    (*newClient)->id     = 0;
-    (*newClient)->logged = 0;
-    memcpy((*newClient)->name, name, length + 1);
+    (*newClient)->socket  = socket;
+    (*newClient)->id      = 0;
+    (*newClient)->logged  = 0;
+    (*newClient)->name[0] = '\0';
 
     return NO_ERROR;
 }
@@ -50,10 +42,19 @@ enum PhiChatErrors DeleteClient(__IN__ struct Client **client)
 }
 
 enum PhiChatErrors Login(__IN__ struct Client *client,
+                         __IN__ const char *name,
                          __IN__ const uint64_t id)
 {
     if (client == NULL)
         return CLIENT_NULL;
+
+    if (name == NULL)
+        return NAME_NULL;
+
+    size_t length = strlen(name);
+
+    if (length >= MAX_LENGTH_NAME)
+        return NAME_TOO_LONG;
 
     if (id == 0)
         return INVALID_ID;
@@ -66,6 +67,7 @@ enum PhiChatErrors Login(__IN__ struct Client *client,
 
     client->logged = 1;
     client->id     = id;
+    memcpy(client->name, name, length + 1);
 
     return NO_ERROR;
 }
@@ -81,8 +83,9 @@ enum PhiChatErrors Logout(__IN__ struct Client *client)
     if (client->id == 0)
         return INVALID_ID;
 
-    client->logged = 0;
-    client->id     = 0;
+    client->logged  = 0;
+    client->id      = 0;
+    client->name[0] = '\0';
 
     return NO_ERROR;
 }
