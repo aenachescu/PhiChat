3ebe796f653216a61ad6099ceebc03848f0c042b
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-10 11:46:25 +0300

    Added GroupTest module

--- test/runtest.sh
+++ test/runtest.sh
@@ -3,6 +3,7 @@
 gcc -std=c99 -o                                 \
     tests Tests.c                               \
     ClientTest.c ../src/Client.c                \
+    GroupTest.c ../src/Group.c                  \
     CUT/src/cut.c                               \
     -I../include -ICUT/include
 
