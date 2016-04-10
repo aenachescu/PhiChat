7325161dc2234d1c8ea565d5bc3a75fd1ce3ad9e
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-10 09:19:04 +0300

    Added NewGroup(), DeleteGroup(), AddClientInGroup(), RemoveClientFromGroup(). Add a new member in struct Group. Added include guard for linux.

--- include/Group.h
+++ include/Group.h
@@ -9,13 +9,30 @@
 #include <Defines.h>
 #include <Errors.h>
 #include <Client.h>
+
+#ifdef __linux
 #include <pthread.h>
+#endif
 
 struct Group
 {
-    unsigned int clientsNumber;
-    struct Client *clients[MAX_CLIENTS_IN_GROUP];
+#ifdef __linux
     pthread_t thread;
+#endif
+
+    unsigned int clientsNumber;
+    uint8_t running;
+    struct Client *clients[MAX_CLIENTS_IN_GROUP];
 };
+
+enum PhiChatErrors NewGroup(__OUT__ struct Group**);
+
+enum PhiChatErrors DeleteGroup(__IN__ struct Group**);
+
+enum PhiChatErrors AddClientInGroup(__IN__ struct Group*,
+                                    __IN__ struct Client*);
+
+enum PhiChatErrors RemoveClientFromGroup(__IN__ struct Group*,
+                                         __IN__ struct Client*);
 
 #endif
