c8329f2f5383c74e18dbc7a151e2e16e7c971268
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-11 20:26:35 +0300

    Added struct Database and the declaration of the helper functions

---
+++ include/Database.h
@@ -0,0 +1,36 @@
+/**
+ * @file Database.h
+ * @brief Database structure, as well as helper function
+ */
+
+#ifndef PhiChat_DATABASE
+#define PhiChat_DATABASE
+
+#include <mysql.h>
+#include <Errors.h>
+
+struct Database
+{
+    MYSQL *sql;
+    char *name;
+    char *host;
+    char *user;
+    char *password;
+};
+
+enum PhiChatErrors NewDatabase(__OUT__ struct Database**,
+                               __IN__  const char*,
+                               __IN__  const char*,
+                               __IN__  const char*,
+                               __IN__  const char*);
+
+enum PhiChatErrors DeleteDatabase(__IN__ struct Database**);
+
+enum PhiChatErrors InitDatabase(__IN__ struct Database*);
+
+enum PhiChatErrors ConnectToDatabase(__IN__ struct Database*);
+
+enum PhiChatErrors DisconnectToDatabase(__IN__ struct Database*);
+
+#endif
+
