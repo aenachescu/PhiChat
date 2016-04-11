73602d534b2f3ba508491a5dbf939a93cc97dbfd
Mihai Borbea <Mihai Borbea>
2016-04-11 22:50:31 +0300

    Implemented NewDatabase function

---
+++ src/Database.c
@@ -0,0 +1,51 @@
+/**
+ * @file Database.h
+ * @brief Implementation of functions of Database structure
+ */
+
+#include <Databse.h>
+
+enum PhiChatErrors NewDatabase(__OUT__ struct Database** db,
+                               __IN__  const char* name,
+                               __IN__  const char* host,
+                               __IN__  const char* user,
+                               __IN__  const char* pw)
+{
+    if (db == NULL)
+        return POINTER_NULL;
+
+    if (name == NULL)
+        DATABASE_NAME_NULL;
+
+    if (host == NULL)
+        DATABASE_HOST_NULL;
+
+    if (user == NULL)
+        DATABASE_USER_NULL;
+
+    if (pw == NULL)
+        DATABASE_PASSWORD_NULL;
+
+    *db = (struct Database *) malloc(sizeof(struct Database));
+
+    if (*db == NULL)
+        return DATABASE_NULL;
+
+    (*db)->sql = mysql_init(NULL);
+
+    if((*db)->sql == NULL)
+    {
+        fprintf(stdout, "%s\n", mysql_error((*db)->sql));
+        return DATABASE_ERROR_INIT
+    }
+
+    (*db)->name = name;
+
+    (*db)->host = host;
+
+    (*db)->user = user;
+
+    (*db)->password = pw;
+
+    return NO_ERROR;
+}
