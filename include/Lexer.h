bb9e380194faf987368fa44e80ef001350ac4e4f
JustBeYou <mihailferaru2000@gmail.com>
2016-04-11 17:32:37 +0300

    Lexer defines added.

---
+++ include/Lexer.h
@@ -0,0 +1,20 @@
+#ifndef PhiChat_LEXER
+#define PhiChat_LEXER
+
+#define PORT                 1
+#define MAX_CONNECTIONS      2
+#define LOG                  3
+#define LOG_FILENAME         4
+#define LOG_DAILY            5
+#define DATABASE             6
+#define DATABASE_HOST        7
+#define DATABASE_USER        8
+#define DATABASE_PASSWORD    9
+#define DATABASE_NAME        10
+#define ASSIGN               11
+#define COLON                12
+#define EXAPND               13
+#define IDENTIFIER           14
+#define INTEGER              15
+
+#endif
