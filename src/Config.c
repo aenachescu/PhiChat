786797ffcc3524c10ac75df58e2ba993e1e164cb
JustBeYou <mihailferaru2000@gmail.com>
2016-04-11 17:48:35 +0300

    Implemented NewConfig and DeletConfig.

--- src/Config.c
+++ src/Config.c
@@ -10,7 +10,18 @@ extern char* yytext;
 
 enum PhiChatErrors NewConfig(__OUT__ struct Config** conf)
 {
+    *conf = malloc(sizeof(struct Config));
+    (*conf)->port = DEFAULT_PORT;
+    (*conf)->maxConnections = DEFAULT_MAX_CONNECTIONS;
+    (*conf)->databaseHost = NULL;
+    (*conf)->databaseUser = NULL;
+    (*conf)->databasePassword = NULL;
+    (*conf)->databaseName = NULL;
+    (*conf)->logfileName = NULL;
+    (*conf)->end = 0;
+    (*conf)->respawn = 0;
 
+    return NO_ERROR;
 }
 
 enum PhiChatErrors DeleteConfig(__IN__ struct Config** conf)
@@ -18,6 +29,13 @@ enum PhiChatErrors DeleteConfig(__IN__ struct Config** conf)
     if (conf == NULL) {
         return POINTER_NULL;
     }
+
+    if (*conf == NULL) {
+        return POINTER_NULL;
+    }
+
+    free(*conf);
+    return NO_ERROR;
 }
 
 enum PhiChatErrors ReadConfig(__IN__ struct Config* conf, __IN__ const char* name)
@@ -25,4 +43,7 @@ enum PhiChatErrors ReadConfig(__IN__ struct Config* conf, __IN__ const char* nam
     if (conf == NULL) {
         return POINTER_NULL;
     }
+
+    FILE *f = fopen(name, "r");
+    fclose(f);
 }
