5b7139de978335530fe3f86f98b9efecfe9f5d3f
JustBeYou <mihailferaru2000@gmail.com>
2016-04-12 18:08:13 +0300

    Implemented NewConfigTest and DeleteConfigTest

--- test/ConfigTest.c
+++ test/ConfigTest.c
@@ -1,14 +1,36 @@
 #include <Config.h>
+#include <Errors.h>
 #include <cut.h>
+#include <stdlib.h>
 
 CUT_DEFINE_TEST(NewConfigTest)
 {
-    CUT_CHECK(1 != 1);
+    CUT_CHECK(NewConfig(NULL) == POINTER_NULL);
+    struct Config* config = NULL;
+    CUT_CHECK(NewConfig(&config) == NO_ERROR);
+    CUT_CHECK(config->port == DEFAULT_PORT);
+    CUT_CHECK(config->respawn == DEFAULT_RESPAWN);
+    CUT_CHECK(config->maxConnections == DEFAULT_MAX_CONNECTIONS);
+    CUT_CHECK(config->databaseHost == NULL);
+    free(config);
 }
 
 CUT_DEFINE_TEST(DeleteConfigTest)
 {
-    CUT_CHECK(1 != 1);
+    struct Config* config = NULL;
+    CUT_CHECK(DeleteConfig(NULL) == POINTER_NULL);
+    CUT_CHECK(DeleteConfig(&config) == CONFIG_NULL);
+    config = malloc(sizeof(struct Config));
+    config->port = 0;
+    config->maxConnections = 0;
+    config->databaseHost = NULL;
+    config->databaseUser = NULL;
+    config->databasePassword = NULL;
+    config->databaseName = NULL;
+    config->logfileName = NULL;
+    config->end = 0;
+    config->respawn = 0;
+    CUT_CHECK(DeleteConfig(&config) == NO_ERROR);
 }
 
 CUT_DEFINE_MODULE(ConfigTest)
