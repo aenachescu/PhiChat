93dff2259d72e6835dfc00e04494e360fbaff391
Mihai Borbea <borbeamihai@hotmail.com>
2016-04-12 12:51:03 +0300

    Grouped the errors.

--- include/Errors.h
+++ include/Errors.h
@@ -10,6 +10,7 @@ enum PhiChatErrors
 {
     NO_ERROR                    =  0,
     POINTER_NULL                = -1,
+
     CLIENT_NULL                 = -2,
     NAME_NULL                   = -3,
     NAME_TOO_LONG               = -4,
@@ -18,23 +19,28 @@ enum PhiChatErrors
     CLIENT_NOT_LOGGED           = -7,
     INVALID_SOCKET              = -8,
     CLIENT_NOT_FOUND            = -9,
+
     GROUP_NULL                  = -10,
     GROUP_NOT_EMPTY             = -11,
     GROUP_STILL_RUNS            = -12,
     GROUP_FULL                  = -13,
     GROUP_EMPTY                 = -14,
+
     NODE_NULL                   = -15,
     NODE_NOT_EMPTY              = -16,
     NODE_FULL                   = -17,
     NODE_EMPTY                  = -18,
+
     LIST_NULL                   = -19,
     LIST_NOT_EMPTY              = -20,
+
     CONFIG_NULL                 = -21,
     CONFIG_UNEXPECTED           = -22,
     CONFIG_EXPECTED_ASSIGN      = -23,
     CONFIG_INTEGER_EXPECTED     = -24,
     CONFIG_EXPECTED_IDENTIFIER  = -25,
     CONFIG_FILENAME_NULL        = -26,
+
     DATABASE_NAME_NULL          = -27,
     DATABASE_HOST_NULL          = -28,
     DATABASE_USER_NULL          = -29,
