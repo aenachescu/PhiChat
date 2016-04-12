f9b67184ff51e7bc0c8d284fd8ab5fb25939cafc
JustBeYou <mihailferaru2000@gmail.com>
2016-04-12 17:18:30 +0300

    Added ConfigTest module.

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,11 +1,12 @@
 # Run tests on Linux
 
-gcc -std=c99 -o                                 \
-    tests Tests.c                               \
-    ClientTest.c ../src/Client.c                \
-    GroupTest.c ../src/Group.c                  \
-    ListClientsTest.c ../src/ListClients.c      \
-    CUT/src/cut.c                               \
+gcc -std=c99 -o                                     \
+    tests Tests.c                                   \
+    ClientTest.c ../src/Client.c                    \
+    GroupTest.c ../src/Group.c                      \
+    ListClientsTest.c ../src/ListClients.c          \
+    ConfigTest.c ../src/Config.c ../src/Lexer.yy.c  \
+    CUT/src/cut.c                                   \
     -I../include -ICUT/include
 
 valgrind ./tests
