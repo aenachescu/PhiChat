01adfff1b8ef90f680a92080f5961783a599f3a6
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-13 11:54:08 +0300

    Added DatabaseTest module. Defined PhiChat_TEST_MODE and linked the tests to mysql lib

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,13 +1,14 @@
 # Run tests on Linux
 
-gcc -std=c99 -o                                     \
+gcc -std=c99 -DPhiChat_TEST_MODE -o                 \
     tests Tests.c                                   \
     ClientTest.c ../src/Client.c                    \
     GroupTest.c ../src/Group.c                      \
     ListClientsTest.c ../src/ListClients.c          \
+    DatabaseTest.c ../src/Database.c                \
     ConfigTest.c ../src/Config.c ../src/Lexer.yy.c  \
     CUT/src/cut.c                                   \
-    -I../include -ICUT/include
+    -I../include -ICUT/include `mysql_config --cflags --libs`
 
 valgrind ./tests
 
