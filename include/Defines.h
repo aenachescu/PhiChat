e64e9895497a093d5e54d49b84f2554445340760
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-13 14:07:27 +0300

    Added ROOM_MAX_LENGTH_NAME. Rearranged the definitions.

--- include/Defines.h
+++ include/Defines.h
@@ -9,10 +9,13 @@
 #define MAX_CLIENTS_IN_GROUP    100
 #define MAX_LENGTH_NAME         19
 #define MAX_CLIENTS_IN_NODE     31
-#define DEFAULT_PORT 12000
+#define ROOM_MAX_LENGTH_NAME    15
+
+#define DEFAULT_PORT            12000
 #define DEFAULT_MAX_CONNECTIONS 100
-#define DEFAULT_RESPAWN 24
+#define DEFAULT_RESPAWN         24
 
 #define __OUT__
 #define __IN__
+
 #endif
