379b6cd51ee012cbd323fbeaa1d71ff39c749b62
EnachescuAlin <alinenachescu033@gmail.com>
2016-04-14 19:39:46 +0300

    Now it's displayed the running duration of the test.

--- test/CUT/include/cut.h
+++ test/CUT/include/cut.h
@@ -2,6 +2,8 @@
 #define C_UNIT_TESTING_FRAMEWORK
 
 #include <stdio.h>
+#include <time.h>
+#include <stdint.h>
 
 #ifdef __linux__
 #define RED     "\x1B[31m"
@@ -27,11 +29,17 @@
     {                                                                           \
         printf("%s: ", #x);                                                     \
         CUT_resetFirstFailedCheck();                                            \
+        struct timespec start, end;                                             \
+        clock_gettime(CLOCK_MONOTONIC, &start);                                 \
         x();                                                                    \
+        clock_gettime(CLOCK_MONOTONIC, &end);                                   \
+        uint64_t sec = end.tv_sec - start.tv_sec;                               \
+        int64_t nsec = end.tv_nsec - start.tv_nsec;                             \
+        long double diff = ((long double)(sec * 1000000000 + nsec) / 1000000);  \
         if (CUT_getFirstFailedCheck() == 0)                                     \
         {                                                                       \
             CUT_incrementPassedTests();                                         \
-            printf("%s%sPASSED%s\n", GREEN, BOLD, NORMAL);                      \
+            printf("%s%sPASSED%s (%Lf ms)\n", GREEN, BOLD, NORMAL, diff);       \
         }                                                                       \
         else                                                                    \
         {                                                                       \
