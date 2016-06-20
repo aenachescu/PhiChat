295650cc95bd0805158fcb97802eba0f260c1101
EnachescuAlin <alinenachescu033@gmail.com>
2016-06-20 18:23:42 +0300

    Created file and added some defines

---
+++ include/Defines.h
@@ -0,0 +1,12 @@
+#ifndef PhiChat_DEFINES
+#define PhiChat_DEFINES
+
+#define CLIENT_MAX_LENGTH_NAME  20
+#define ROOM_MAX_LENGTH_NAME    20
+#define MAX_CLIENTS_IN_GROUP    100
+
+#define PORT                    12000
+#define MAX_CONNECTIONS         100
+
+#endif
+
