c2205c9d50f64e8bf4d9abe9ef75c24d62a20491
EnachescuAlin <alinenachescu033@gmail.com>
2016-06-24 18:27:58 +0300

    Added CLIENT_MAX_ROOMS

--- include/Defines.h
+++ include/Defines.h
@@ -1,6 +1,7 @@
 #ifndef PhiChat_DEFINES
 #define PhiChat_DEFINES
 
+#define CLIENT_MAX_ROOMS        20
 #define CLIENT_MAX_LENGTH_NAME  20
 #define ROOM_MAX_LENGTH_NAME    20
 #define MAX_CLIENTS_IN_GROUP    100
