87b1c796e3f8aeaa18b435086f69f370ad9f7def
EnachescuAlin <alinenachescu033@gmail.com>
2016-06-24 15:35:19 +0300

    Added MAX_CLIENTS_IN_NODE

--- include/Defines.h
+++ include/Defines.h
@@ -4,6 +4,7 @@
 #define CLIENT_MAX_LENGTH_NAME  20
 #define ROOM_MAX_LENGTH_NAME    20
 #define MAX_CLIENTS_IN_GROUP    100
+#define MAX_CLIENTS_IN_NODE     32
 
 #define PORT                    12000
 #define MAX_CONNECTIONS         100
